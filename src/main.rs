9af5a4df06c44530c6f73061af67315aa092dba9
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2023-05-27 12:37:24 +0200

    Initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use serde_json::Value;
+
+struct Payload {
+    ohlc: Vec<Vals>,
+}
+
+
+struct Vals {
+    open: u8,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.avanza.se/_api/price-chart/stock/5364?timePeriod=one_week";
+    let res = reqwest::get(url)
+        .await?
+        .json::<Value>()
+        .await?;
+
+    println!("{:#?}", res);
+    Ok(())
 }
