ba673a454fea8df62b1709c973021ebe49910400
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2023-05-27 13:36:03 +0200

    Second commit

--- src/get_stock_data.rs
+++ src/get_stock_data.rs
@@ -1,9 +1,15 @@
-mod get_stock_data;
-use std::collections::HashMap;
+use reqwest::{get, Client};
+
 use serde_json::Value;
 
+
+async fn get_stock() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.avanza.se/_api/search/global-search";
+    let res = ""
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn get_stock_data() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://www.avanza.se/_api/price-chart/stock/5364?timePeriod=one_week";
     let res = reqwest::get(url)
         .await?
